273d8fc609e5f188696eefa0f15d2cc4f24a6108
eduardo-hdez <ehdez2004@gmail.com>
2025-10-30 18:24:49 -0600

    [Build] add getInboxRequests controller.

--- src/modules/tecbooks/routes/register.routes.js
+++ src/modules/tecbooks/routes/register.routes.js
@@ -1,13 +1,11 @@
-import express from "express"
-import emailRateLimiter from "../middlewares/rate-limiter.middleware.js"
-import verifyJWT from "../middlewares/token-verification.middleware.js"
-import registerControllers from "../controllers/register.controller.js"
+import express from "express";
+import emailRateLimiter from "../middlewares/rate-limiter.middleware.js";
+import verifyJWT from "../middlewares/token-verification.middleware.js";
+import registerControllers from "../controllers/register.controller.js";
 
-const router = express.Router()
+const router = express.Router();
 
-router.get("/get-institutions", (req, res) => {
-
-})  
+router.get("/get-institutions", (req, res) => {});
 
 // STUDENT REGISTRY ROUTES: request -> verify -> finalize
 router.post("/student/request", emailRateLimiter, (req, res) => {
@@ -27,24 +25,30 @@ router.post("/student/finalize", verifyJWT, (req, res) => {
   // 1. Verify JWT middleware
   // 2. parse body (name, password, token)
   // 3. Call create user use-case
-  // 4. generate 
+  // 4. generate
 });
 
-// PROFESSOR REGISTRY ROUTES: 
+// PROFESSOR REGISTRY ROUTES:
 // 1. request -> (approved by admin) -> finalize
 // 2. join-from-invite
-router.post("/professor/request", emailRateLimiter, registerControllers.createProfessorRequest);
+router.post(
+  "/professor/request",
+  emailRateLimiter,
+  registerControllers.createProfessorRequest
+);
 
 router.get("/professor/verify-invite-token", verifyJWT, (req, res) => {
-    // 1. Verify JWT middleware
-    // 4. generate new token with (exp for 1 hour)
-    // 5. return new token (email, institution, exp)
+  // 1. Verify JWT middleware
+  // 4. generate new token with (exp for 1 hour)
+  // 5. return new token (email, institution, exp)
 });
 
-router.post("/professor/finalize", emailRateLimiter, async (req, res) => {
-    
-});
+router.post("/professor/finalize", emailRateLimiter, async (req, res) => {});
 
-router.post("/admin/invite-professor", verifyJWT)
+router.post("/admin/accept-professor-request", verifyJWT, (req, res) => {});
 
-export default router
+router.post("/admin/decline-professor-request", verifyJWT, (req, res) => {});
+
+router.post("/admin/invite-professor", verifyJWT, (req, res) => {});
+
+export default router;
